7fd44e34026b3e3917c23ff141cbde102b41f20f
toitolucho <antoniomolina.yampa@gmail.com>
2022-03-16 03:47:10 -0400

    adding images to documentation

--- database scripts/HospitalTables.sql
+++ database scripts/HospitalTables.sql
@@ -24,8 +24,6 @@ create table Hospitals
 	CONSTRAINT PK_Hospital PRIMARY KEY (HospitalId)
 )
 
-
-
 CREATE TABLE Patients
 (
 	PatientId	INT IDENTITY,
@@ -52,18 +50,6 @@ CREATE TABLE Doctors
 	CONSTRAINT PK_Doctors PRIMARY KEY(DoctorId),
 	CONSTRAINT FK_Doctors_Hospitals FOREIGN KEY(HospitalId) REFERENCES Hospitals(HospitalId)
 )
-insert into Doctors values ('Rodolfo', 'Merida' , '2018-01-01','Rdolofo Villarroel 68', null,  1)
-insert into Doctors values ('Alejandra', 'Rojas' , '2018-01-01','Rdolofo Villarroel 68', null, 1)
-insert into Doctors values ('Romseri', 'Lopez' , '2018-01-01','Rdolofo Villarroel 68', null, 2)
-insert into Doctors values ('Romelia', 'Carreño' , '2018-01-01','Rdolofo Villarroel 68', null, 2)
-insert into Doctors values ('Carlos Walter', 'Baptista' , '2018-01-01','Rdolofo Villarroel 68', null, 3)
-select * from Doctors
-
-
-insert into Hospitals values ('Hospital de las Americas', 'Hospital de las Americas')
-insert into Hospitals values ('Hospital Cristo Sol', 'risto Sol')
-insert into Hospitals values ('Hospital Universitario', 'Hospital de las Americas')
-select * from Hospitals
 
 CREATE TABLE PatienteHistorial
 (
@@ -76,7 +62,6 @@ CREATE TABLE PatienteHistorial
 )
 
 
-
 CREATE TABLE Specialities
 (
 	SpecialityId	INT IDENTITY,
@@ -85,37 +70,3 @@ CREATE TABLE Specialities
 	CONSTRAINT PK_Specialities PRIMARY KEY (SpecialityId),
 	CONSTRAINT U_SpecialityName UNIQUE (SpecialityName)
 )
-
-
-select * from doctors
-
-
-
---drop  table Students
-
---CREATE TABLE Students
---(
---	Id	INT IDENTITY,
---	name		VARCHAR(200),
---	email	VARCHAR(200),
---	dob	DATE,	
---	age int,
---	CONSTRAINT PK_Students PRIMARY KEY(Id)
-	
---)
-
-
---insert into Students values ('Sanuel', 'samuel18@gmail.com', '2018-01-01',14)
---insert into Students values ('Soraya', 'soraya18@gmail.com', '2018-01-09',16)
---insert into Students values ('alejandro', 'alejandro@gmail.com', '2018-01-09',19)
-
-select
-        doctor0_.DoctorId as doctorid1_0_,
-        doctor0_.Address as address2_0_,
-        doctor0_.DateOfBirth as dateofbi3_0_,
-        --doctor0_.hospital as hospital4_0_,
-        doctor0_.LastNames as lastname5_0_,
-        doctor0_.FirstName as firstnam6_0_,
-        doctor0_.PictureFile as picturef7_0_ 
-    from
-        Doctors doctor0_
---
+++ database scripts/InitialData.sql
@@ -0,0 +1,20 @@
+use hospitalDH
+go
+
+
+insert into Hospitals values ('Hospital de las Americas', 'Hospital de las Americas')
+insert into Hospitals values ('Hospital Cristo Sol', 'risto Sol')
+insert into Hospitals values ('Hospital Universitario', 'Hospital de las Americas')
+go
+
+insert into Doctors values ('Rodolfo', 'Merida' , '2008-01-01','Avenida de las Americas', 'https://bootdey.com/img/Content/avatar/avatar1.png',  1)
+insert into Doctors values ('Alejandra', 'Rojas' , '2008-01-01','Rodolofo Villarroel 68', 'https://bootdey.com/img/Content/avatar/avatar2.png', 1)
+insert into Doctors values ('Romseri', 'Lopez' , '2000-01-01','Esteban Loayza 35', 'https://bootdey.com/img/Content/avatar/avatar3.png', 2)
+insert into Doctors values ('Romelia', 'Carreño' , '2001-01-01','Loa 65', 'https://bootdey.com/img/Content/avatar/avatar4.png', 2)
+insert into Doctors values ('Carlos Walter', 'Baptista' , '2002-01-01','Filipos 35', 'https://bootdey.com/img/Content/avatar/avatar5.png', 3)
+insert into Doctors values ('Luis Antonio', 'Molina' , '1986-01-01','Aguilar 25', 'https://bootdey.com/img/Content/avatar/avatar6.png', 2)
+insert into Doctors values ('Samira', 'Molina Lopez' , '1990-01-28','Rosendo 85', 'https://bootdey.com/img/Content/avatar/avatar7.png', 3)
+
+
+
+
